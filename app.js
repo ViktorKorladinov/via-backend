892557c820b750a6fb2613fb87d1503bf7c1fabe
ViktorKorladinov <v.korladinov@gmail.com>
2023-01-11 09:23:37 +0100

    updated swagger

--- app.js
+++ app.js
@@ -10,15 +10,16 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 
 const app = express()
-const whitelist = ['http://localhost:3000']
+// const whitelist = ['http://localhost:3000']
 const corsOptions = {
     credentials: true,
     origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+        callback(null,true)
+        // if (whitelist.indexOf(origin) !== -1) {
+        //     callback(null, true)
+        // } else {
+        //     callback(new Error('Not allowed by CORS'))
+        // }
     }
 }
 app.use(cors(corsOptions))
